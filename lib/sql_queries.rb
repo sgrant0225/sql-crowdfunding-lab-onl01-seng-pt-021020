121e6b970e6f9a3fd757bf9dde52a5b1500327e7
Sharnell Grant <sharnellgrant@gmail.com>
2020-04-13 19:09:30 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -48,6 +48,7 @@ def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_b
   "SELECT projects.name, SUM(pledges.amount)
     FROM projects
      JOIN pledges
-      ON 
+      ON projects.id = pledges.project_id
+       WHERE project.category = '
   "
 end
